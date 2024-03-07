91f1f7048cdb50d5da1132720606b4166df3ae15
Jean Ponchon <jean.ponchon@novadiscovery.com>
2024-03-07 01:08:54 +0100

    feat: add colorscheme

---
+++ color-scheme/campbell.nix
@@ -0,0 +1,20 @@
+{
+  name = "Campbell";
+  reference = "https://docs.microsoft.com/en-us/windows/terminal/customize-settings/color-schemes#campbell";
+  black = "#0c0c0c";
+  red = "#c50f1f";
+  green = "#13a10e";
+  yellow = "#c19c00";
+  blue = "#0037da";
+  magenta = "#881798";
+  cyan = "#3a96dd";
+  white = "#cccccc";
+  blackBright = "#767676";
+  redBright = "#e74856";
+  greenBright = "#16c60c";
+  yellowBright = "#f9f1a5";
+  blueBright = "#3b78ff";
+  magentaBright = "#b4009e";
+  cyanBright = "#61d6d6";
+  whiteBright = "#f2f2f2";
+}
--- home-manager/sway.nix
+++ home-manager/sway.nix
@@ -1,11 +1,60 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+let
+  colorScheme = import ../color-scheme/campbell.nix;
+in
+{
   wayland.windowManager.sway = {
     enable = true;
     config = rec {
       modifier = "Mod4";
-      
+
       # Use kitty as default terminal
       terminal = "kitty";
+
+      gaps = {
+        smartBorders = "on";
+        smartGaps = true;
+      };
+      floating.border = 1;
+      window.border = 1;
+      bars = [ ];
+      colors = {
+        focused = {
+          background = colorScheme.green;
+          border = colorScheme.greenBright;
+          childBorder = colorScheme.green;
+          indicator = colorScheme.green;
+          text = colorScheme.black;
+        };
+        focusedInactive = {
+          background = colorScheme.yellow;
+          border = colorScheme.yellowBright;
+          childBorder = colorScheme.yellow;
+          indicator = colorScheme.yellow;
+          text = colorScheme.black;
+        };
+        unfocused = {
+          background = colorScheme.magenta;
+          border = colorScheme.magentaBright;
+          childBorder = colorScheme.magenta;
+          indicator = colorScheme.magenta;
+          text = colorScheme.black;
+        };
+        urgent = {
+          background = colorScheme.red;
+          border = colorScheme.redBright;
+          childBorder = colorScheme.red;
+          indicator = colorScheme.red;
+          text = colorScheme.black;
+        };
+        background = colorScheme.black;
+      };
+      fonts = {
+        names = [ "Roboto" ];
+        style = "Regular Bold";
+        size = 12.0;
+      };
+
 
       startup = [
         # Launch Chrome on start
@@ -26,6 +75,13 @@
           click_method = "clickfinger";
         };
       };
+
+
+      # Remoe the titlebar
+      window.titlebar = false;
+
+      # Enable auto back and forth between workspaces: redo the last navigation to switch to the previous workspaceS
+      workspaceAutoBackAndForth = true;
 
       # Extends the default keybindings with mkOptionDefault
       keybindings = lib.mkOptionDefault {
