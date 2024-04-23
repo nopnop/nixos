23e9e4524ebd5df9a05b6f3e0cc633ce2f699d45
Jean Ponchon <jean.ponchon@novadiscovery.com>
2024-04-23 16:03:10 +0200

    chore: change starship conf

--- home-manager/starship.nix
+++ home-manager/starship.nix
@@ -2,7 +2,20 @@
   programs.starship = {
     enable = true;
     settings = {
-     
+      git_status = {
+        conflicted = "🏳";
+        ahead = "🚀\${count}";
+        behind = "😰 ⇣\${behind_count}";
+        diverged = "😵 ⇡\${ahead_count}⇣\${behind_count}";
+        up_to_date = "✓";
+        untracked = "🤷";
+        stashed = "📦";
+        modified = "📝";
+        staged = "[ ++\${count} ] (green)";
+        renamed = "👅";
+        deleted = "🗑";
+
+      };
     };
   };
 }
