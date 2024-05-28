41e47ea78ab206d45b25f354ba85bc9c4f088198
Jean Ponchon <jean.ponchon@novadiscovery.com>
2024-05-28 14:17:13 +0200

    chore: prepare waybar with swaync

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -1,6 +1,5 @@
 { config, lib, pkgs, ... }:
-let
-  colorScheme = import ../color-scheme/catppuccin.nix;
+let colorScheme = import ../color-scheme/catppuccin.nix;
 in
 {
   # See <https://github.com/nix-community/home-manager/blob/master/modules/programs/waybar.nix>
@@ -8,6 +7,30 @@ in
   # see <https://github.com/Alexays/Waybar/wiki/Examples>
   programs.waybar = {
     enable = true;
-    # settings = { };
+    # settings = {
+    #   mainBar = {
+    #     "custom/notification" = {
+    #       tooltip = false;
+    #       format = "{icon}";
+    #       format-icons = {
+    #         notification = "<span foreground='red'><sup></sup></span>";
+    #         none = "";
+    #         dnd-notification = "<span foreground='red'><sup></sup></span>";
+    #         dnd-none = "";
+    #         inhibited-notification = "<span foreground='red'><sup></sup></span>";
+    #         inhibited-none = "";
+    #         dnd-inhibited-notification = "<span foreground='red'><sup></sup></span>";
+    #         dnd-inhibited-none = ";";
+    #       };
+    #       return-type = "json";
+    #       exec-if = "which swaync-client";
+    #       exec = "swaync-client -swb";
+    #       on-click = "swaync-client -t -sw";
+    #       on-click-right = "swaync-client -d -sw";
+    #       escape = true;
+    #     };
+    #   };
+    # };
   };
 }
+
